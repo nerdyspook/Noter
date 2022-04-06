203a94bd25a7f7b6af38969121b9fd63e8df1d70
Susanto <nerdyspook007@gmail.com>
2022-04-07 03:52:30 +0530

    added authentication

---
+++ src/reducers/AuthReducer.js
@@ -0,0 +1,16 @@
+export const AuthReducer = (stateAuth, actionAuth) => {
+    switch (actionAuth.type) {
+        case "GET_USER_DETAILS":
+            return {
+                ...stateAuth,
+                userDetails: [actionAuth.payload],
+                isAuth: true,
+            };
+
+        case "USER_LOGOUT":
+            return { ...stateAuth, userDetails: [], isAuth: false };
+
+        default:
+            return stateAuth;
+    }
+};
