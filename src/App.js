69d874be81e620037ed660cef8164798d62d04d4
Susanto <nerdyspook007@gmail.com>
2022-04-06 18:38:32 +0530

    feat: added login and signup page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { Route, Routes } from "react-router-dom";
+import Login from "./Pages/Auth/Login";
+import Signup from "./Pages/Auth/Signup";
 import Landing from "./Pages/Landing/Landing";
 
 const App = () => {
@@ -6,6 +8,8 @@ const App = () => {
         <div className="App">
             <Routes>
                 <Route path="/" element={<Landing />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
             </Routes>
         </div>
     );
